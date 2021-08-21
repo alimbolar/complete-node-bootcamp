f1b2e4af60d473bb4fb5b92e8d37f052dd07ba1d
Alim Bolar <alim@fourplusmedia.com>
2021-08-21 07:28:31 +0400

    completed nested routes

--- 4-natours/starter/controllers/reviewController.js
+++ 4-natours/starter/controllers/reviewController.js
@@ -2,35 +2,35 @@ const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/AppError');
 const Review = require('./../models/reviewModel');
 
+exports.getAllReviews = catchAsync(async function (req, res, next) {
+  let filter = {};
+  if (req.params.tourId) filter = { tour: req.params.tourId };
 
-
-exports.getAllReviews = catchAsync(async function(req, res, next) {
-  const review = await Review.find();
+  const review = await Review.find(filter);
 
   res.status(200).json({
     status: 'success',
     result: review.length,
     data: {
-      review
-    }
+      review,
+    },
   });
 });
 
-exports.createOneReview = catchAsync(async function(req, res, next) {
-
-  if(!req.body.tour) req.body.tour = req.params.tourId;
-  if(!req.body.user) req.body.user = req.user.id // sent from the protect middleware
+exports.createOneReview = catchAsync(async function (req, res, next) {
+  if (!req.body.tour) req.body.tour = req.params.tourId;
+  if (!req.body.user) req.body.user = req.user.id; // sent from the protect middleware
   const review = await Review.create({
     review: req.body.review,
     rating: req.body.rating,
     createdAt: req.body.createdAt,
     tour: req.body.tour,
-    user: req.body.user
+    user: req.body.user,
   });
 
   res.status(200).json({
     status: 'success',
     message: 'Review Done',
-    review
+    review,
   });
 });
