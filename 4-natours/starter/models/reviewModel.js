59d879d33fe2ae8ec8769e18df3740ca841c09a7
Alim Bolar <alim@fourplusmedia.com>
2021-08-27 08:11:30 +0400

    Average Rating, Unique Indexing and Setter Done

--- 4-natours/starter/models/reviewModel.js
+++ 4-natours/starter/models/reviewModel.js
@@ -1,48 +1,90 @@
 const mongoose = require('mongoose');
+const Tour = require('./../models/tourModel');
 
 const reviewSchema = new mongoose.Schema(
   {
     review: {
       type: String,
-      required: [true, 'Review is a mandatory field']
+      required: [true, 'Review is a mandatory field'],
     },
     rating: {
       type: Number,
       min: 1,
       max: 5,
-      required: [true, 'Rating is mandatory']
+      required: [true, 'Rating is mandatory'],
     },
     createdAt: {
       type: Date,
-      default: Date.now()
+      default: Date.now(),
     },
-    tour:{
-        type: mongoose.Schema.ObjectId,
-        ref: 'Tour',
-        require: [true, 'Tour is required']
-      },
-    user:{
-        type: mongoose.Schema.ObjectId,
-        ref: 'User',
-        required: [true, 'User is required']
-      }
-  
+    tour: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'Tour',
+      require: [true, 'Tour is required'],
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+      required: [true, 'User is required'],
+    },
   },
   {
     toJSON: { virtuals: true },
-    toObject: { virtuals: true }
+    toObject: { virtuals: true },
   }
 );
 
-reviewSchema.pre(/^find/, function(next) {
+reviewSchema.pre(/^find/, function (next) {
+  // this.populate({
+  //   path: 'tour',
+  //   select: 'name',
+  // })
   this.populate({
-    path: 'tour',
-    select: 'name'
-  }).populate({
-    path : 'user',
-    select : 'name'
-  })
+    path: 'user',
+    select: 'name',
+  });
   next();
+});
+
+reviewSchema.statics.calcAverageRating = async function (tourId) {
+  const stats = await this.aggregate([
+    { $match: { tour: tourId } },
+    {
+      $group: {
+        _id: '$tour',
+        nRating: { $sum: 1 },
+        avgRating: { $avg: '$rating' },
+      },
+    },
+  ]);
+
+  console.log(stats);
+  if (stats.length > 0) {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsQuantity: stats[0].nRating,
+      ratingsAverage: stats[0].avgRating,
+    });
+  } else {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsQuantity: 0,
+      ratingsAverage: 4.5,
+    });
+  }
+};
+
+reviewSchema.post('save', function () {
+  this.constructor.calcAverageRating(this.tour);
+});
+
+reviewSchema.pre(/^findOneAnd/, async function (next) {
+  this.r = await this.findOne();
+
+  console.log('document from query', this.r);
+  next();
+});
+
+reviewSchema.post(/^findOneAnd/, async function () {
+  await this.r.constructor.calcAverageRating(this.r.tour);
 });
 
 const Review = mongoose.model('Review', reviewSchema);
