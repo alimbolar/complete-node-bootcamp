ccd2ee868153a20b4a388d17e8aabf8d6791e2f8
Alim Bolar <alim@fourplusmedia.com>
2021-09-04 08:23:11 +0400

    pug overview page done

--- 4-natours/starter/controllers/viewController.js
+++ 4-natours/starter/controllers/viewController.js
@@ -1,8 +1,25 @@
-exports.getOverview = (req, res) => {
+const Tour = require('./../models/tourModel');
+const catchAsync = require('./../utils/catchAsync');
+
+exports.getOverview = catchAsync(async (req, res,next) => {
+  // 1) Get tour data from the collection
+  // 2) Build the template
+  // 3) Render template with data from step 1
+
+  // STEP 1
+  const tours = await Tour.find();
+
+  // STEP 2
+  // This happens in the PUG template
+
+  // STEP 3
+
     res.status(200).render('overview',{
-        title : "All Tours"
+        title : "All Tours",
+        tours
+        
     });
-  }
+  });
 
 exports.getTour = (req, res) => {
     res.status(200).render('tour',{
